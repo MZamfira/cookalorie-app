3ddf6d5b55d3e88b7552094899b379999c4722b6
MZamfira <mihai.cosminzam@gmail.com>
2025-04-13 16:17:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "7e499a2f-a17d-45a5-8452-b4a17e271c82.lovableproject.com",
     port: 8080,
   },
   plugins: [
